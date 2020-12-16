6a5a13e798b9be4dfbf9981e3fbdf63f0aa1f425
Robin Ghosh <roghos@microsoft.com>
2020-12-16 15:59:39 -0500

    Added multithread/callback on completion

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -18,6 +18,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 return func.HttpResponse("Bad Request",status_code=400)
             else:
                 return func.HttpResponse(result, status_code=200)
+
+        else:
+            result1 = service.thread_work(req_body,dothread)
+            if result1 is "Success":
+                return func.HttpResponse("Successfully queued our work process",status_code=202)
+            else:
+                return func.HttpResponse("Bad Request", status_code=400)
+           
     else:
         return func.HttpResponse(
              "Please pass an attribute called dothread [Y/N]",status_code=400
--- HttpTrigger1/sqlserverbroker.py
+++ HttpTrigger1/sqlserverbroker.py
@@ -2,6 +2,10 @@ import logging
 import pyodbc
 import collections
 import json
+import threading
+import time
+import requests
+
 
 server = 'your-sql-server.database.windows.net'
 database = 'your-sql-db'
@@ -21,8 +25,7 @@ def exec_sql(req_body,dothread):
 
     conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = conn.cursor()
-    #cursor.execute(sql)
-    cursor.execute("SELECT id,store_id,merchant_id,trantype,refnum FROM dbo.Transactions")
+    cursor.execute(sql)
     rows = cursor.fetchall()
     
     # Convert query to row array and pump out json
@@ -30,14 +33,42 @@ def exec_sql(req_body,dothread):
     objects_list = []
     for row in rows:
         d = collections.OrderedDict()
-        d['id'] = row.id
-        d['store_id'] = row.store_id
-        d['merchant_id'] = row.merchant_id
-        d['refnum'] = row.refnum
-        d['trantype'] = row.trantype
+        d['id'] = row.PONumber
+        d['store_id'] = row.name
+        d['merchant_id'] = row.addressLine1
+        d['refnum'] = row.addressLine2
+        d['trantype'] = row.addressLine3
         objects_list.append(d)
 
+    # the print() and logging.info() statement will not produce any output under the context of a thread run
+    if dothread == "Y" :
+        time.sleep(1)
+        callback_uri= req_body[0]['data']['callback_uri'] 
+        if(not callback_uri):
+            logging.info("The callback_uri is empty")
+        else:
+            print("No it is not empty, so send a message back to the caller")
+            payload = {"id":"5555","product_id":"string","name": "robin ghosh","description": "desc","purpose": "pur","imagelink": "img"}
+            headers = {"authorization": "2213"}
+            r = requests.post(callback_uri,data=payload, headers=headers)
+            s=r.text
+            
+    print('Dumping the lists')
     return json.dumps(objects_list)
+
+
+
+#
+# Wrap the actual atomic code as a callable threa
+#
+def thread_work(req_body,dothread):
+# start threads by passing function to Thread constructor
+    t = threading.Thread(target=exec_sql, args=(req_body,dothread)) 
+    logging.info("Python HTTP trigger function started a worker thread with name of {}".format(threading.current_thread().name))
+    t.start() 
+    logging.info("Completed the thread..")
+    return "Success"
+
 
 
 
